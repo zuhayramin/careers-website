500a4e0a2de2711e6e8ea1b987a52c17c0cde77a
Zuhayr Amin <44747492+zuhayramin@users.noreply.github.com>
2023-12-06 13:28:26 +0400

    Add data to page using templates

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+        'id':1,
+        'title': 'Data Analyst',
+        'location': 'Riyadh, KSA',
+        'salary': 'SR 70,000' 
+    },
+    {
+        'id':2,
+        'title': 'Fronend Engineer',
+        'location': 'Dubai, UAE',
+        'salary': 'AED 190,000' 
+    },
+    {
+        'id':3,
+        'title': 'Full Stack Developer',
+        'location': 'Paris, France',
+        'salary': 'Euro 50,000' 
+    },
+    {
+        'id':4,
+        'title': 'DevOps Engineer',
+        'location': 'Stockholm, Sweden',
+        'salary': 'Euro 320,000' 
+    }
+]
+
 @app.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('home.html', jobs = JOBS)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', debug = True)
